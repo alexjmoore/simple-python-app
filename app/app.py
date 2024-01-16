52480306612649349f9c1687c6325c65e599abbf
Alex Moore <alexmoore@google.com>
2024-01-16 17:15:17 +0000

    fix env detection and add some logging@

--- app/app.py
+++ app/app.py
@@ -1,8 +1,7 @@
 import logging
 from os import environ
-from socket import gethostname
 from waitress import serve
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from urllib.request import Request, urlopen
 from urllib.error import URLError
 
@@ -19,11 +18,13 @@ def index():
     instance_name = ""
     instance_zone = ""
 
+
+
     if "GAE_APPLICATION" in environ:
         environment="Running on Google App Engine!"
     elif environ.get('K_SERVICE'):
         environment="Running on Cloud Run!"
-    elif gethostname().endswith('.internal'):
+    elif is_on_gce():
         environment="Running on Google Compute Engine!"
     else:
         environment="Not running on Google Cloud (or cannot be determined)!"
@@ -44,6 +45,7 @@ def index():
 # helper function - generates the requested status code
 @app.route('/status/<code>')
 def status(code):
+    logging.debug(f"Returning HTTP Status Code: {code} for {request.remote_addr} with {request.headers.get('User-Agent')}")
     return ("Returned HTTP Status Code: " + code, code)
 
 def queryMetadata(attribute):
@@ -53,7 +55,17 @@ def queryMetadata(attribute):
         return urlopen(req).read().decode()
     except URLError as e:
         return "Metadata entry not found."
+    
+def is_on_gce():
+    try:
+        response = requests.get(
+            "http://metadata.google.internal/computeMetadata/v1/instance/hostname",
+            headers={"Metadata-Flavor": "Google"}
+        )
+        return response.status_code == 200  # Successful response
+    except requests.exceptions.RequestException:
+        return False  # Probably not on GCE
 
 if __name__ == "__main__":
-    logging.getLogger('waitress').setLevel(logging.INFO)
+    logging.getLogger('waitress').setLevel(logging.DEBUG)
     serve(app, host='0.0.0.0', port=environ.get('PORT', '8080'))
